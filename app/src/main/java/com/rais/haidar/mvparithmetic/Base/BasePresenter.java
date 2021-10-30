77970dbd93c79490774f9bc6ea122e83f04cac57
Saliim <32667011+Saliim11@users.noreply.github.com>
2021-10-30 11:20:45 +0700

    Update BasePresenter.java
    mo nanya

--- app/src/main/java/com/rais/haidar/mvparithmetic/Base/BasePresenter.java
+++ app/src/main/java/com/rais/haidar/mvparithmetic/Base/BasePresenter.java
@@ -3,7 +3,7 @@ package com.rais.haidar.mvparithmetic.Base;
 /**
  * Created by Hoidar on 10/11/18.
  */
-
+//bang bang ini buat apaan bang yak?
 public interface BasePresenter <T extends BaseView> {
     void onAttach(T v);
 }
